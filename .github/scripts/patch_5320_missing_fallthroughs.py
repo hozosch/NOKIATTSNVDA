05d1f11a58147e99bb0c51998e852db2ab60c7fc
Karl Eick <hozosch@web.de>
2026-09-01 01:05:22 +0200

    Restore missing frontend variant path in test 17

--- .github/scripts/patch_5320_missing_fallthroughs.py
+++ .github/scripts/patch_5320_missing_fallthroughs.py
@@ -166,4 +166,183 @@ if source.count(old_lower_exit_target) != 1:
 	raise SystemExit("missing or ambiguous 0x830ecbc2 target marker")
 source = source.replace(old_lower_exit_target, new_lower_exit_target, 1)
 
+old_variant_case = """    case 0x830dbbceu: goto L_830dbbce;
+    case 0x830dbbeeu: goto L_830dbbee;
+"""
+new_variant_case = """    case 0x830dbbceu: goto L_830dbbce;
+    case 0x830dbbd0u: goto L_830dbbd0;
+    case 0x830dbbd2u: goto L_830dbbd2;
+    case 0x830dbbd4u: goto L_830dbbd4;
+    case 0x830dbbd6u: goto L_830dbbd6;
+    case 0x830dbbd8u: goto L_830dbbd8;
+    case 0x830dbbdau: goto L_830dbbda;
+    case 0x830dbbdcu: goto L_830dbbdc;
+    case 0x830dbbe0u: goto L_830dbbe0;
+    case 0x830dbbe2u: goto L_830dbbe2;
+    case 0x830dbbe4u: goto L_830dbbe4;
+    case 0x830dbbe6u: goto L_830dbbe6;
+    case 0x830dbbe8u: goto L_830dbbe8;
+    case 0x830dbbeau: goto L_830dbbea;
+    case 0x830dbbecu: goto L_830dbbec;
+    case 0x830dbbeeu: goto L_830dbbee;
+"""
+if source.count(old_variant_case) != 1:
+	raise SystemExit("missing or ambiguous 0x830dbbd0 dispatch marker")
+source = source.replace(old_variant_case, new_variant_case, 1)
+
+old_variant_branch = """L_830dbbba:
+    nokia_frontend_last_pc=0x830dbbbau;
+    u_132e00 = ((uint64_t)((reg_ZR & UINT64_C(0xff)) == (UINT64_C(0) & UINT64_C(0xff)))) & UINT64_C(0xff);
+    if ((u_132e00 & UINT64_C(0xff))) return NOKIA_FRONTEND_UNSUPPORTED;
+    goto L_830dbbbc;
+"""
+new_variant_branch = """L_830dbbba:
+    nokia_frontend_last_pc=0x830dbbbau;
+    u_132e00 = ((uint64_t)((reg_ZR & UINT64_C(0xff)) == (UINT64_C(0) & UINT64_C(0xff)))) & UINT64_C(0xff);
+    if ((u_132e00 & UINT64_C(0xff))) goto L_830dbbd0;
+    goto L_830dbbbc;
+"""
+if source.count(old_variant_branch) != 1:
+	raise SystemExit("missing or ambiguous 0x830dbbba branch marker")
+source = source.replace(old_variant_branch, new_variant_branch, 1)
+
+old_variant_target = "L_830dbbee:\n"
+new_variant_target = """L_830dbbd0:
+    nokia_frontend_last_pc=0x830dbbd0u;
+    u_150a00 = ((uint64_t)((reg_r4 & UINT64_C(0xffffffff)) + (UINT64_C(32) & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    reg_r1 = ((uint64_t)(nokia_mem_load(&machine, (u_150a00 & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xffffffff);
+    goto L_830dbbd2;
+L_830dbbd2:
+    nokia_frontend_last_pc=0x830dbbd2u;
+    reg_tmpCY = ((uint64_t)((reg_r1 & UINT64_C(0xffffffff)) <= (reg_r0 & UINT64_C(0xffffffff)))) & UINT64_C(0xff);
+    reg_tmpOV = ((uint64_t)(nokia_sborrow((reg_r0 & UINT64_C(0xffffffff)), (reg_r1 & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xff);
+    reg_r0 = ((uint64_t)((reg_r0 & UINT64_C(0xffffffff)) - (reg_r1 & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    reg_tmpNG = ((uint64_t)(nokia_sext((reg_r0 & UINT64_C(0xffffffff)), 4) < nokia_sext((UINT64_C(0) & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xff);
+    reg_tmpZR = ((uint64_t)((reg_r0 & UINT64_C(0xffffffff)) == (UINT64_C(0) & UINT64_C(0xffffffff)))) & UINT64_C(0xff);
+    reg_CY = ((uint64_t)((reg_tmpCY & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_ZR = ((uint64_t)((reg_tmpZR & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_NG = ((uint64_t)((reg_tmpNG & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_OV = ((uint64_t)((reg_tmpOV & UINT64_C(0xff)))) & UINT64_C(0xff);
+    goto L_830dbbd4;
+L_830dbbd4:
+    nokia_frontend_last_pc=0x830dbbd4u;
+    u_163100 = ((uint64_t)((UINT64_C(13) & UINT64_C(0xffffffff)) - (UINT64_C(1) & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    u_163200 = ((uint64_t)(nokia_shl((reg_r0 & UINT64_C(0xffffffff)), (u_163100 & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xffffffff);
+    u_163400 = ((uint64_t)((u_163200 & UINT64_C(0xffffffff)) & (UINT64_C(2147483648) & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    u_163500 = ((uint64_t)((UINT64_C(13) & UINT64_C(0xffffffff)) == (UINT64_C(0) & UINT64_C(0xffffffff)))) & UINT64_C(0xff);
+    u_163600 = ((uint64_t)((u_163500 & UINT64_C(0xff)) && (reg_CY & UINT64_C(0xff)))) & UINT64_C(0xff);
+    u_163700 = ((uint64_t)((UINT64_C(13) & UINT64_C(0xffffffff)) != (UINT64_C(0) & UINT64_C(0xffffffff)))) & UINT64_C(0xff);
+    u_163800 = ((uint64_t)((u_163400 & UINT64_C(0xffffffff)) != (UINT64_C(0) & UINT64_C(0xffffffff)))) & UINT64_C(0xff);
+    u_163900 = ((uint64_t)((u_163700 & UINT64_C(0xff)) && (u_163800 & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_tmpCY = ((uint64_t)((u_163600 & UINT64_C(0xff)) || (u_163900 & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_r0 = ((uint64_t)(nokia_shl((reg_r0 & UINT64_C(0xffffffff)), (UINT64_C(13) & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xffffffff);
+    reg_tmpNG = ((uint64_t)(nokia_sext((reg_r0 & UINT64_C(0xffffffff)), 4) < nokia_sext((UINT64_C(0) & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xff);
+    reg_tmpZR = ((uint64_t)((reg_r0 & UINT64_C(0xffffffff)) == (UINT64_C(0) & UINT64_C(0xffffffff)))) & UINT64_C(0xff);
+    reg_CY = ((uint64_t)((reg_tmpCY & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_ZR = ((uint64_t)((reg_tmpZR & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_NG = ((uint64_t)((reg_tmpNG & UINT64_C(0xff)))) & UINT64_C(0xff);
+    goto L_830dbbd6;
+L_830dbbd6:
+    nokia_frontend_last_pc=0x830dbbd6u;
+    u_164a00 = ((uint64_t)((UINT64_C(16) & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    u_163f00 = ((uint64_t)((u_164a00 & UINT64_C(0xffffffff)) - (UINT64_C(1) & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    u_164000 = ((uint64_t)(nokia_shr((reg_r0 & UINT64_C(0xffffffff)), (u_163f00 & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xffffffff);
+    u_164200 = ((uint64_t)((u_164000 & UINT64_C(0xffffffff)) & (UINT64_C(1) & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    u_164300 = ((uint64_t)((u_164a00 & UINT64_C(0xffffffff)) == (UINT64_C(0) & UINT64_C(0xffffffff)))) & UINT64_C(0xff);
+    u_164400 = ((uint64_t)((u_164300 & UINT64_C(0xff)) && (reg_CY & UINT64_C(0xff)))) & UINT64_C(0xff);
+    u_164500 = ((uint64_t)((u_164a00 & UINT64_C(0xffffffff)) != (UINT64_C(0) & UINT64_C(0xffffffff)))) & UINT64_C(0xff);
+    u_164600 = ((uint64_t)((u_164200 & UINT64_C(0xffffffff)) != (UINT64_C(0) & UINT64_C(0xffffffff)))) & UINT64_C(0xff);
+    u_164700 = ((uint64_t)((u_164500 & UINT64_C(0xff)) && (u_164600 & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_tmpCY = ((uint64_t)((u_164400 & UINT64_C(0xff)) || (u_164700 & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_r1 = ((uint64_t)(nokia_shr((reg_r0 & UINT64_C(0xffffffff)), (UINT64_C(16) & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xffffffff);
+    reg_tmpNG = ((uint64_t)(nokia_sext((reg_r1 & UINT64_C(0xffffffff)), 4) < nokia_sext((UINT64_C(0) & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xff);
+    reg_tmpZR = ((uint64_t)((reg_r1 & UINT64_C(0xffffffff)) == (UINT64_C(0) & UINT64_C(0xffffffff)))) & UINT64_C(0xff);
+    reg_CY = ((uint64_t)((reg_tmpCY & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_ZR = ((uint64_t)((reg_tmpZR & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_NG = ((uint64_t)((reg_tmpNG & UINT64_C(0xff)))) & UINT64_C(0xff);
+    goto L_830dbbd8;
+L_830dbbd8:
+    nokia_frontend_last_pc=0x830dbbd8u;
+    reg_r0 = ((uint64_t)((reg_r4 & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    reg_tmpNG = ((uint64_t)(nokia_sext((reg_r0 & UINT64_C(0xffffffff)), 4) < nokia_sext((UINT64_C(0) & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xff);
+    reg_tmpZR = ((uint64_t)((reg_r0 & UINT64_C(0xffffffff)) == (UINT64_C(0) & UINT64_C(0xffffffff)))) & UINT64_C(0xff);
+    reg_ZR = ((uint64_t)((reg_tmpZR & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_NG = ((uint64_t)((reg_tmpNG & UINT64_C(0xff)))) & UINT64_C(0xff);
+    goto L_830dbbda;
+L_830dbbda:
+    nokia_frontend_last_pc=0x830dbbdau;
+    reg_tmpCY = ((uint64_t)(nokia_carry((reg_r0 & UINT64_C(0xffffffff)), (UINT64_C(16) & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xff);
+    reg_tmpOV = ((uint64_t)(nokia_scarry((reg_r0 & UINT64_C(0xffffffff)), (UINT64_C(16) & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xff);
+    reg_r0 = ((uint64_t)((reg_r0 & UINT64_C(0xffffffff)) + (UINT64_C(16) & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    reg_tmpNG = ((uint64_t)(nokia_sext((reg_r0 & UINT64_C(0xffffffff)), 4) < nokia_sext((UINT64_C(0) & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xff);
+    reg_tmpZR = ((uint64_t)((reg_r0 & UINT64_C(0xffffffff)) == (UINT64_C(0) & UINT64_C(0xffffffff)))) & UINT64_C(0xff);
+    reg_CY = ((uint64_t)((reg_tmpCY & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_ZR = ((uint64_t)((reg_tmpZR & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_NG = ((uint64_t)((reg_tmpNG & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_OV = ((uint64_t)((reg_tmpOV & UINT64_C(0xff)))) & UINT64_C(0xff);
+    goto L_830dbbdc;
+L_830dbbdc:
+    nokia_frontend_last_pc=0x830dbbdcu;
+    reg_lr = ((uint64_t)((UINT64_C(2198715360) & UINT64_C(0xffffffff)) | (UINT64_C(1) & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    reg_TB = ((uint64_t)((UINT64_C(1) & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_lr = UINT64_C(2198715361);
+    reg_pc=UINT64_C(2198717090); return NOKIA_FRONTEND_CONTINUE;
+L_830dbbe0:
+    nokia_frontend_last_pc=0x830dbbe0u;
+    reg_r4 = ((uint64_t)((reg_r0 & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    reg_tmpNG = ((uint64_t)(nokia_sext((reg_r4 & UINT64_C(0xffffffff)), 4) < nokia_sext((UINT64_C(0) & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xff);
+    reg_tmpZR = ((uint64_t)((reg_r4 & UINT64_C(0xffffffff)) == (UINT64_C(0) & UINT64_C(0xffffffff)))) & UINT64_C(0xff);
+    reg_ZR = ((uint64_t)((reg_tmpZR & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_NG = ((uint64_t)((reg_tmpNG & UINT64_C(0xff)))) & UINT64_C(0xff);
+    goto L_830dbbe2;
+L_830dbbe2:
+    nokia_frontend_last_pc=0x830dbbe2u;
+    reg_r3 = ((uint64_t)((reg_sp & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    goto L_830dbbe4;
+L_830dbbe4:
+    nokia_frontend_last_pc=0x830dbbe4u;
+    u_150c00 = ((uint64_t)((reg_r3 & UINT64_C(0xffffffff)) + (UINT64_C(8) & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    u_15b500 = ((uint64_t)(nokia_mem_load(&machine, (u_150c00 & UINT64_C(0xffffffff)), 2))) & UINT64_C(0xffff);
+    reg_r1 = ((uint64_t)((u_15b500 & UINT64_C(0xffff)))) & UINT64_C(0xffffffff);
+    goto L_830dbbe6;
+L_830dbbe6:
+    nokia_frontend_last_pc=0x830dbbe6u;
+    u_150a00 = ((uint64_t)((reg_r6 & UINT64_C(0xffffffff)) + (UINT64_C(0) & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    reg_r0 = ((uint64_t)(nokia_mem_load(&machine, (u_150a00 & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xffffffff);
+    goto L_830dbbe8;
+L_830dbbe8:
+    nokia_frontend_last_pc=0x830dbbe8u;
+    reg_tmpCY = ((uint64_t)(nokia_carry((reg_r0 & UINT64_C(0xffffffff)), (reg_r1 & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xff);
+    reg_tmpOV = ((uint64_t)(nokia_scarry((reg_r0 & UINT64_C(0xffffffff)), (reg_r1 & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xff);
+    reg_r0 = ((uint64_t)((reg_r0 & UINT64_C(0xffffffff)) + (reg_r1 & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    reg_tmpNG = ((uint64_t)(nokia_sext((reg_r0 & UINT64_C(0xffffffff)), 4) < nokia_sext((UINT64_C(0) & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xff);
+    reg_tmpZR = ((uint64_t)((reg_r0 & UINT64_C(0xffffffff)) == (UINT64_C(0) & UINT64_C(0xffffffff)))) & UINT64_C(0xff);
+    reg_CY = ((uint64_t)((reg_tmpCY & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_ZR = ((uint64_t)((reg_tmpZR & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_NG = ((uint64_t)((reg_tmpNG & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_OV = ((uint64_t)((reg_tmpOV & UINT64_C(0xff)))) & UINT64_C(0xff);
+    goto L_830dbbea;
+L_830dbbea:
+    nokia_frontend_last_pc=0x830dbbeau;
+    reg_tmpCY = ((uint64_t)((UINT64_C(1) & UINT64_C(0xffffffff)) <= (reg_r0 & UINT64_C(0xffffffff)))) & UINT64_C(0xff);
+    reg_tmpOV = ((uint64_t)(nokia_sborrow((reg_r0 & UINT64_C(0xffffffff)), (UINT64_C(1) & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xff);
+    reg_r0 = ((uint64_t)((reg_r0 & UINT64_C(0xffffffff)) - (UINT64_C(1) & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    reg_tmpNG = ((uint64_t)(nokia_sext((reg_r0 & UINT64_C(0xffffffff)), 4) < nokia_sext((UINT64_C(0) & UINT64_C(0xffffffff)), 4))) & UINT64_C(0xff);
+    reg_tmpZR = ((uint64_t)((reg_r0 & UINT64_C(0xffffffff)) == (UINT64_C(0) & UINT64_C(0xffffffff)))) & UINT64_C(0xff);
+    reg_CY = ((uint64_t)((reg_tmpCY & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_ZR = ((uint64_t)((reg_tmpZR & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_NG = ((uint64_t)((reg_tmpNG & UINT64_C(0xff)))) & UINT64_C(0xff);
+    reg_OV = ((uint64_t)((reg_tmpOV & UINT64_C(0xff)))) & UINT64_C(0xff);
+    goto L_830dbbec;
+L_830dbbec:
+    nokia_frontend_last_pc=0x830dbbecu;
+    u_150a00 = ((uint64_t)((reg_r6 & UINT64_C(0xffffffff)) + (UINT64_C(0) & UINT64_C(0xffffffff)))) & UINT64_C(0xffffffff);
+    if (!nokia_mem_store(&machine, (u_150a00 & UINT64_C(0xffffffff)), (reg_r0 & UINT64_C(0xffffffff)), 4)) return NOKIA_FRONTEND_UNSUPPORTED;
+    goto L_830dbbee;
+L_830dbbee:
+"""
+if source.count(old_variant_target) != 1:
+	raise SystemExit("missing or ambiguous 0x830dbbd0 target marker")
+source = source.replace(old_variant_target, new_variant_target, 1)
+
 path.write_text(source, encoding="utf-8", newline="\n")
